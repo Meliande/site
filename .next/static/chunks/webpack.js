b3c212f851b6277b47d0a8259facefb2e911633d
Rodrigo Meliande <rodrigomeliande@msn.com>
2022-11-15 11:25:52 -0300

    lint fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "707dea6288c59539"; }
+/******/ 		__webpack_require__.h = function() { return "2482fcda19a3bd3d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -10,21 +10,21 @@ export default function AboutComponent() {
             <div className="grid grid-cols-5 gap-2">
                 {
                     fronSkills.map(skill => (
-                        <div className="bg-[#B4CEB3] rounded-lg">
+                        <div key={skill} className="bg-[#B4CEB3] rounded-lg">
                             {skill}
                         </div>
                     ))
                 }
                 {
                     backSkills.map(skill => (
-                        <div className="bg-[#88A0A8] rounded-lg">
+                        <div key={skill} className="bg-[#88A0A8] rounded-lg">
                             {skill}
                         </div>
                     ))
                 }
                 {
                     softSkills.map(skill => (
-                        <div className="bg-[#FAD4D8] rounded-lg">
+                        <div key={skill} className="bg-[#FAD4D8] rounded-lg">
                             {skill}
                         </div>
                     ))
