33a65018d77c0c0a50c29cff8f9a0a553c1ea79c
Rodrigo Meliande <rodrigomeliande@msn.com>
2022-11-16 08:58:07 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,20 @@ import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return(
-    <div className='h-screen'>
+    <div className='h-screen text-center'>
         <Sidebar/>
-        <div className="p-20 grid grid-cols-1 place-items-center text-center">
+        <div className="flex-col p-20">
                 <h1 className="text-5xl font-bold">Olá, eu sou Rodrigo Meliande</h1>
-                <p className="pt-9 text-xl break-words px-96">
+                <p className="pt-9 text-2xl">
                     Um desenvolvedor web e mobile que busca ajudar no sucesso da sua companhia através das aplicações que desenvolve
                 </p>
-                <Link className="pt-10" href="/projects">
-                    <button className="p-5 font-bold bg-zinc-500 rounded-2xl hover:scale-110 hover:text-amber-200 transition duration-300">PROJETOS</button>
-                </Link>
+                <div className="pt-20">
+                    <Link
+                        href="/projects"
+                    >
+                        <button className="bg-zinc-500 rounded-lg p-5">PROJETOS</button>
+                    </Link>
+                </div>
         </div>
     </div>
 )
